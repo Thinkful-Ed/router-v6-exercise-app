5450f78eafee89462b40d994b789a7db8c621934
Adan Rodriguez-Jones <138242165+Adanjones@users.noreply.github.com>
2024-02-22 22:00:32 -0500

    Update User.js

--- src/User.js
+++ src/User.js
@@ -1,21 +1,20 @@
 import React from 'react'
-2import UserProfile from './UserProfile';
-3import { Outlet, Link, useParams } from 'react-router-dom';
-4
-5function User() {
-6    const { userId } = useParams();
-7
-8    return (
-9    <>
-10        <UserProfile />
-11        <hr/>
-12        <Link to={`/users/${userId}/posts`}>View All Posts</Link>
-13        ||
-14        <Link to={`/users/${userId}/posts/1`}>View Post with ID 1</Link>
-15        <hr/>
-16        <Outlet />
-17    </>
-18    )
-19}
-20
-21export default User
+import UserProfile from './UserProfile';
+import { Outlet, Link, useParams } from 'react-router-dom';
+
+function User() {
+    const { userId } = useParams();
+
+    return (
+    <>
+        <UserProfile />
+        <hr/>
+        <Link to={`/users/${userId}/posts`}>View All Posts</Link>
+        ||
+        <Link to={`/users/${userId}/posts/1`}>View Post with ID 1</Link>
+        <hr/>
+        <Outlet />
+    </>
+  )
+}
+export default User
